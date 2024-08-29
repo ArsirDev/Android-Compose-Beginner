6acad0e5058b8ec25d6f0d2a6c30fe072841de9e
Arfan Siregar <arfan.siregar@finansia.com>
2024-08-29 21:58:12 +0700

    Rename MainActivity to RowActivity

--- app/src/main/java/com/arsir/androidcomposebeginner/RowActivity.kt
+++ app/src/main/java/com/arsir/androidcomposebeginner/RowActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.arsir.androidcomposebeginner.ui.theme.AndroidComposeBeginnerTheme
 
-class MainActivity : ComponentActivity() {
+class RowActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
